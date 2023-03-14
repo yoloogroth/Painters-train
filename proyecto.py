105111337b3b0de2d7e28f6f28b1e1e7940ee08c
Yolo <Yolo051@outlook.es>
2023-03-14 09:14:30 -0600

    update version01

---
+++ proyecto.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import streamlit as st
+import re
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+st.set_page_config(page_title="painters train", page_icon=":sun:")
+st.markdown("<h1 style='text-align: center; color: purple;'>PAINTERS TRAIN</h1>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; color: Cyan;'>YOLOTZIN GROTH HERNANDEZ</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; color: LightGreen;'>zs20020311</h3>", unsafe_allow_html=True)
