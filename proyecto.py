769db7c6fbc069f71466e176e515845d6d29e8b4
Yolo <Yolo051@outlook.es>
2023-03-15 14:36:31 -0600

    version1.2

--- proyecto.py
+++ proyecto.py
@@ -6,7 +6,7 @@ import plotly.express as px
 
 st.set_page_config(page_title="painters train", page_icon=":sun:")
 st.markdown("<h1 style='text-align: center; color: yellow;'>PAINTERS TRAIN</h1>", unsafe_allow_html=True)
-st.markdown("<h5 style='text-align: center; color: Cyan;'> Create by:YOLOTZIN GROTH  S20020311</h5>", unsafe_allow_html=True)
+st.markdown("<h5 style='text-align: center; color: black;'> Create by: YOLOTZIN GROTH  S20020311</h5>", unsafe_allow_html=True)
 
 
 DATA_URL = 'https://raw.githubusercontent.com/yoloogroth/Painters-train/master/all_data_info.csv '
